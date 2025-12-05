1737cd0de1ab01ed6a6c7ecf46fe720445709025
adamrose <xx155@georgetown.edu>
2025-12-05 00:40:46 -0500

    update the R scripts

--- model/multinomial_model.R
+++ model/multinomial_model.R
@@ -6,19 +6,24 @@ library(tidyverse)
 library(lme4)
 library(broom)
 library(readr)
+library(stargazer)
 
 #############################################
 ### 1. Read data
 #############################################
 
-df <- read_csv("model_variables.csv")
+df <- read_csv("model_variables_new.csv")
 cat("Loaded:", nrow(df), "rows\n")
+
+df <- subset(df, year>2016)
+table(df$year)
 
 #############################################
 ### 2. Filter rows with error
 #############################################
 
 err <- df %>% filter(error_flag == 1)
+no_err <- df %>% filter(error_flag == 0)
 
 #############################################
 ### 3. Identify element columns
@@ -55,36 +60,42 @@ clean_name <- function(x){
     str_replace("_$","")
 }
 
-indicator_names <- paste0("err_", clean_name(top15))
+err_element_names <- paste0("err_", clean_name(top15))
 
 for(i in seq_along(top15)){
   elem <- top15[i]
-  name <- indicator_names[i]
+  name <- err_element_names[i]
   err[[name]] <- apply(err[element_cols], 1, function(r){
     as.integer(elem %in% r)
   })
 }
+names(err)
+table(err$err_contributions)
 
 #############################################
 ### 6. Correct feature lists to match your data
 #############################################
 
+#add: 
 features_cont <- c(
   "fsusize",
-  "fsnetinc"
+  "fsnetinc",
+  "lastcert"
 )
 
+#add: expedser
+#explore adding authrep
 features_bin <- c(
   "children_present",
   "elderly_present",
   "fsndis",
-  "abwdst1"
+  "abwdst1",
+  "expedser"
 )
 
 features_cat <- c(
   "actntype...38",
   "cat_elig",
-  "state...44",
   "year"
 )
 
@@ -94,9 +105,15 @@ features <- c(features_cont, features_bin, features_cat)
 ### 7. Select needed columns
 #############################################
 
-needed <- c(features, indicator_names)
+needed <- c(features, err_element_names, "state...44")
 
-err_model <- err %>%
+err_and_no_err.df <- bind_rows(err, no_err)
+names(err_and_no_err.df)
+table(is.na(err_and_no_err.df[,107:121]))
+err_and_no_err.df[,107:121][is.na(err_and_no_err.df)[,107:121]] <- 0
+table(is.na(err_and_no_err.df[,107:121]))
+
+err_model <- err_and_no_err.df %>%
   select(all_of(needed)) %>%
   drop_na()
 
@@ -115,9 +132,13 @@ err_model <- err_model %>%
 
 logit_results <- list()
 
-for(target in indicator_names){
+for(target in err_element_names){
   
-  formula_str <- paste0(target, " ~ ", paste(features, collapse=" + "))
+  formula_str <- paste0(
+    target, " ~ log(lastcert + 1) + ",
+    paste(c(features_cont[-3], features_bin, features_cat), collapse = " + "),
+    " + state...44"
+  )
   formula_obj <- as.formula(formula_str)
   
   m <- glm(formula_obj, data=err_model, family=binomial)
@@ -127,17 +148,20 @@ for(target in indicator_names){
   print(summary(m))
 }
 
+stargazer(logit_results[1:15], type='html', out="element_results.html")
+
+
 #############################################
 ### 10. Run random-slope glmer
 #############################################
 
 glmer_results <- list()
 
-total_models <- length(indicator_names)
+total_models <- length(err_element_names)
 
-for (i in seq_along(indicator_names)) {
+for (i in seq_along(err_element_names)) {
   
-  target <- indicator_names[i]
+  target <- err_element_names[i]
   
   cat("\n===========================================\n")
   cat("Running model", i, "of", total_models, ":", target, "\n")
@@ -181,3 +205,4 @@ for (i in seq_along(indicator_names)) {
   cat("End time:", as.character(end_time), "\n")
   cat("Elapsed:", round(as.numeric(elapsed), 2), "seconds\n")
 }
+
